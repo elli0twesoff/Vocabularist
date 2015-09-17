bc46812fdcea8b35b4623ea685226f2bbab3662c
Elliot Wesoff <elliot@techrockstars.com>
2015-09-17 02:36:11 -0400

    started setting up for processing stripe webhooks.  added disclaimer in sign up page.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -36,6 +36,43 @@ class PaymentsController < ApplicationController
     respond_with(@payment)
   end
 
+  def webooks
+    # thank god stripe provided all this cool stuff.  i'm just wayyy too lazy fo dat.
+    Payment.process_stripe_webhook(params)
+  
+    # Use Stripe's bindings...
+  rescue Stripe::CardError => e
+    # Since it's a decline, Stripe::CardError will be caught
+    body = e.json_body
+    err  = body[:error]
+
+    puts "Status is: #{e.http_status}"
+    puts "Type is: #{err[:type]}"
+    puts "Code is: #{err[:code]}"
+    # param is '' in this case
+    puts "Param is: #{err[:param]}"
+    puts "Message is: #{err[:message]}"
+  rescue Stripe::RateLimitError => e
+    # Too many requests made to the API too quickly
+    puts "Stripe error: #{e.message}"
+  rescue Stripe::InvalidRequestError => e
+    # Invalid parameters were supplied to Stripe's API
+    puts "Stripe error: #{e.message}"
+  rescue Stripe::AuthenticationError => e
+    # Authentication with Stripe's API failed
+    # (maybe you changed API keys recently)
+    puts "Stripe error: #{e.message}"
+  rescue Stripe::APIConnectionError => e
+    # Network communication with Stripe failed
+    puts "Stripe error: #{e.message}"
+  rescue Stripe::StripeError => e
+    # Display a very generic error to the user, and maybe send
+    # yourself an email
+    puts "Stripe error: #{e.message}"
+  rescue => e
+    # Something else happened, completely unrelated to Stripe
+  end
+
   private
     def set_payment
       @payment = Payment.find(params[:id])
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,7 @@
 class Payment < ActiveRecord::Base
+
+  def self.process_stripe_webhook(params)
+    puts "Received a stripe webhook: \n#{params}"
+  end
+
 end
