8ae7ea81ff1b25ca30a7c04eb3852181af2b0a44
Elliot Wesoff <elliot@techrockstars.com>
2015-09-17 01:27:37 -0400

    restrict access to only chapter 1 if the user is not signed in with an active account or is not an admin.

--- app/controllers/vocab_tests_controller.rb
+++ app/controllers/vocab_tests_controller.rb
@@ -1,21 +1,25 @@
 class VocabTestsController < ApplicationController
   def start
-		if params[:chapters]
-			if params[:commit] == 'start'
+    if params[:chapters]
+      if params[:commit] == 'start'
 
-				@words = EnglishWord.get_test_words(params)
-				@words_json = EnglishWord.get_json_words(@words)
+        if !current_user || !current_user.admin?
+          params[:chapters].each do |chap|
+            if chap.to_i > 1
+              redirect_to root_path, notice: "please sign in with an activated account to use chapters beyond chapter 1. thank you."
+            end
+          end
+        end
 
-			elsif params[:commit] == 'deutsch'
-				# collect german words here...
-			else
-				redirect_to root_path, alert: 'hmmm......'
-			end
+        @words = EnglishWord.get_test_words(params)
+        @words_json = EnglishWord.get_json_words(@words)
+      else
+        redirect_to root_path, alert: 'hmmm......'
+      end
 
-		else
-			# for now redirect to the home page.  we'll redirect to a results page
-			# once we get it built.
-			redirect_to root_path, alert: 'you need to select at least one chapter to be tested on!'
-		end
+    else
+
+      redirect_to root_path, alert: 'you need to select at least one chapter to be tested on!'
+    end
   end
 end
