9a83d77087d6d2ffa1814518af2c7407247a4450
Elliot Wesoff <elliot@techrockstars.com>
2015-09-27 21:14:03 -0400

    working on getting devise controller to redirect to the payment page after signup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 
-  resources :payments
+  #resources :payments
   get 'german_words/load_from_file', to: 'german_words#load_from_file'
   put 'german_words/update_words', to: 'german_words#update_words'
 	post 'vocab_tests/start', to: 'vocab_tests#start'
 	post 'vocab_tests/complete_test', to: 'vocab_tests#complete_test'
   get 'payments/new', to: 'payments#new', as: 'subscribe'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+
+  devise_scope :user do
+    post 'pay' => 'users/registrations#create', as: 'payment'
+  end
+
   resources :german_words
   resources :english_words
 
