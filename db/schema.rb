8bc276cab9bbda626f71ee7ec67d0ed5582bf812
Elliot Wesoff <elliot@techrockstars.com>
2015-10-09 02:48:33 -0400

    added first and last name to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928024529) do
+ActiveRecord::Schema.define(version: 20151009064210) do
 
   create_table "english_words", force: true do |t|
     t.string   "word"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20150928024529) do
     t.boolean  "admin",                  default: false
     t.string   "stripe_id"
     t.boolean  "activated",              default: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
